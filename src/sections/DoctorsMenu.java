20cec7c7d2ecf0e3874d5cff22ecbaf8692d339d
kshenukaishan <kshenukaishan@outlook.com>
2024-02-27 22:18:05 +0530

    Initialized doctors menu and menu items

---
+++ src/sections/DoctorsMenu.java
@@ -0,0 +1,23 @@
+package sections;
+
+import models.Doctor;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class DoctorsMenu {
+
+    public static void doctorSection(List<Doctor> doctors, Scanner input){
+
+        String[] doctorsMenuItems = new String[6];
+
+        doctorsMenuItems[0] = "(1) Add Doctor";
+        doctorsMenuItems[1] = "(2) Find Doctor";
+        doctorsMenuItems[2] = "(3) Update Doctor";
+        doctorsMenuItems[3] = "(4) Delete Doctor";
+        doctorsMenuItems[4] = "(5) View All Doctors";
+        doctorsMenuItems[5] = "(6) Exit";
+
+    }
+
+}
