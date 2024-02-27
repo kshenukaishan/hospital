de4dd8474eee6754574ebd5f81f9ec4da1faf1a9
kshenukaishan <kshenukaishan@outlook.com>
2024-02-27 21:36:01 +0530

    Initialize the Doctor model

---
+++ src/models/Doctor.java
@@ -0,0 +1,50 @@
+package models;
+
+public class Doctor {
+
+    private int id;
+    private String name;
+    private String specialty;
+
+    public Doctor() {
+    }
+
+    public Doctor(int id, String name, String specialty) {
+        this.id = id;
+        this.name = name;
+        this.specialty = specialty;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSpecialty() {
+        return specialty;
+    }
+
+    public void setSpecialty(String specialty) {
+        this.specialty = specialty;
+    }
+
+    @Override
+    public String toString() {
+        return "Doctor{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", specialty='" + specialty + '\'' +
+                '}';
+    }
+}
