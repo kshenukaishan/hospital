064c5402e149b706909679ab64ac9cbde178b8f5
kshenukaishan <kshenukaishan@outlook.com>
2024-02-27 22:23:41 +0530

    initialized appointment section crud methods

--- src/sections/AppointmentMenu.java
+++ src/sections/AppointmentMenu.java
@@ -18,6 +18,49 @@ public class AppointmentMenu {
         appointmentsMenuItems[4] = "(5) View All DAppointments";
         appointmentsMenuItems[5] = "(6) Exit";
 
+        while(true){
+
+            int selectedMenuItem = MainMenu.printMenu(appointmentsMenuItems, input);
+
+            switch (selectedMenuItem){
+                case 0:
+                    addAppointment(appointments, input);
+                    break;
+                case 1:
+                    findAppointmentById(appointments, input);
+                    break;
+                case 2:
+                    updateAppointment(appointments, input);
+                    break;
+                case 3:
+                    deleteAppointment(appointments, input);
+                    break;
+                case 4:
+                    viewAllAppointments(appointments);
+                    break;
+                case 5:
+                    return;
+            }
+        }
     }
 
+    private static void viewAllAppointments(List<Appointment> appointments) {
+        System.out.println("View all appointment");
+    }
+
+    private static void deleteAppointment(List<Appointment> appointments, Scanner input) {
+        System.out.println("Delete appointment");
+    }
+
+    private static void updateAppointment(List<Appointment> appointments, Scanner input) {
+        System.out.println("Update appointment");
+    }
+
+    private static void findAppointmentById(List<Appointment> appointments, Scanner input) {
+        System.out.println("Find appointment by id");
+    }
+
+    private static void addAppointment(List<Appointment> appointments, Scanner input) {
+        System.out.println("Add appointment");
+    }
 }
