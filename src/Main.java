5604896c42052ff92580dbf48fdc862cd2a25149
kshenukaishan <kshenukaishan@outlook.com>
2024-02-28 11:46:16 +0530

    remove main menu message

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to Hospital Management System");
 
         Scanner input = new Scanner(System.in);
         System.out.println("Welcome to the Hospital Management System.");
