7b7624cb860f5d07b3696863a251f4f39a73adb1
kshenukaishan <kshenukaishan@outlook.com>
2024-03-03 15:27:15 +0530

    added methods to BoImpl classes

--- src/bo/custom/impl/AppointmentBoImpl.java
+++ src/bo/custom/impl/AppointmentBoImpl.java
@@ -2,5 +2,33 @@ package bo.custom.impl;
 
 import bo.custom.AppointmentBo;
 
+import java.util.List;
+import java.util.Scanner;
+
 public class AppointmentBoImpl implements AppointmentBo {
+
+    @Override
+    public void viewAll(List list) {
+
+    }
+
+    @Override
+    public void delete(List list, Scanner input) {
+
+    }
+
+    @Override
+    public void update(List list, Scanner input) {
+
+    }
+
+    @Override
+    public void findById(List list, Scanner input) {
+
+    }
+
+    @Override
+    public void add(List list, Scanner input) {
+
+    }
 }
--- src/bo/custom/impl/PatientBoImpl.java
+++ src/bo/custom/impl/PatientBoImpl.java
@@ -2,5 +2,33 @@ package bo.custom.impl;
 
 import bo.custom.PatientBo;
 
+import java.util.List;
+import java.util.Scanner;
+
 public class PatientBoImpl implements PatientBo {
+
+    @Override
+    public void viewAll(List list) {
+
+    }
+
+    @Override
+    public void delete(List list, Scanner input) {
+
+    }
+
+    @Override
+    public void update(List list, Scanner input) {
+
+    }
+
+    @Override
+    public void findById(List list, Scanner input) {
+
+    }
+
+    @Override
+    public void add(List list, Scanner input) {
+
+    }
 }
